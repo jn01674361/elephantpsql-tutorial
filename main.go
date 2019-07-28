e498c0133b979dbd65f51d3679e7950d67b1dcce
jn01674361 <joar.nykvist@gmail.com>
2019-07-28 17:17:06 +0200

    Initialize repo

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+// psql uri: postgres://qoxyqwbm:FZAING3jqt-er6DMYrm71OvJZoYE7ys2@balarama.db.elephantsql.com:5432/qoxyqwbm
+
+import (
+	"github.com/eaigner/jet"
+    "github.com/lib/pq"
+    "log"
+    "os"
+)
+
+func logFatal(err error) {
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+func list_db(){
+
+}
+
+func main(){
+
+}
